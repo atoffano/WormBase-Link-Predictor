66b2a74453f95092394c96a5c98a4233d2210943
Christophe Héligon <christophe.heligon@univ-rennes1.fr>
2024-04-05 17:08:49 +0200

    log data extracted to dict + better figure layout

--- reports/create_report.py
+++ reports/create_report.py
@@ -3,33 +3,91 @@ import argparse
 import os
 import matplotlib.pyplot as plt
 
-
-def figure(file):
-    """
-    extracts information from file, generates a graph and saves it as file.png
-    """
+def data_extractor(file:str) -> dict:
+    """Extracts information from log file"""
+    
+    nodes = 0
+    relation_types = 0
+    triples = 0
     embedding_stats= list()
-    hit_at_1 = ''
-    batch_size = ''
+    train_loss = list()
+    valid_loss = list()
 
     pattern = " - Epoch "
 
     with open(file, 'r') as f:
         for line in f:
-            if pattern in line:
+            if 'Start time:' in line:
+                start_time = line.split(':')[-1].strip()
+            elif 'Number of entities' in line:
+                entities = int(line.split(':')[-1].strip())
+                if nodes == 0:
+                    nodes = entities
+                elif nodes != entities:
+                    nodes = -1
+            elif 'Number of relation types' in line:
+                reltype = int(line.split(':')[-1].strip())
+                if relation_types == 0:
+                    relation_types = reltype
+                elif relation_types != reltype:
+                    relation_types = -1
+            elif 'Number of triples' in line:
+                triples += int(line.split(':')[-1].strip())
+            elif 'method :' in line:
+                method = line.split(':')[-1].strip()
+            elif pattern in line:
                 embedding_stats.append(line.strip())
             elif "Hit@1 :" in line:
-                hit_at_1 = line.split('INFO:')[-1].strip()
+                hit_at_1 = float(line.split(':')[-1].strip())
             elif "batch_size : " in line:
-                batch_size = line.split(':')[-1].strip()
-
-    train_loss = list()
-    valid_loss = list()
+                batch_size = int(line.split(':')[-1].strip())
+            elif 'n_epochs :' in line:
+                epochs = int(line.split(':')[-1].strip())
+            elif 'keywords :' in line:
+                keys = line.split(':')[-1].strip()
+                if keys != 'None':
+                    keywords = line.split(' ').strip()
+                    if 'onto' in keywords:
+                        ontologies = True
+                    else:
+                        ontologies = False
+                    if 'direct' in keywords:
+                        structure = 'Direct'
+                    else:
+                        structure = 'Indirect'
+                else:
+                    keywords = False
+                    structure = 'Unknown'
+                    ontologies = 'Unknown'
 
     for element in embedding_stats:
         stats = element.split('|')[1]
         train_loss.append(float(stats.split(' ')[3].strip(',')))
         valid_loss.append(float(stats.split(' ')[6].strip()))
+    
+    best_epoch = valid_loss.index(min(valid_loss)) +1
+
+    data = {'start': start_time,
+            'nodes':nodes,
+            'relation_types':relation_types,
+            'triples':triples,
+            'algorithm':method,
+            'train_loss': train_loss,
+            'valid_loss':valid_loss,
+            'hit@1':hit_at_1,
+            'batch_size':batch_size,
+            'data':keywords,
+            'ontologies': ontologies,
+            'structure':structure,
+            'epochs':epochs,
+            'best_epoch':best_epoch,
+            }
+    return data
+
+def figure(file:str, hit1:float, batch:int, train_loss:list, valid_loss:list, best_epoch:int) -> None:
+    """
+    Generates a train/validation graph and saves it as file.png
+    """
 
     #train line
     xt = list(range(1, len(train_loss)+1, 1))
@@ -40,10 +98,12 @@ def figure(file):
     fig, ax = plt.subplots()
     ax.plot(xt, yt, label = "training loss")
     ax.plot(xv, yv, label = "validation loss")
-    #plot Hit @1 value
-    plt.text(0.3, 0.25, hit_at_1, transform = ax.transAxes)
     #plot batch size
-    plt.text(0.3, 0.3,'Batch_size = ' + batch_size, transform = ax.transAxes) #horizontalalignment='center', verticalalignment='center', transform = ax.transAxes
+    plt.text(0.5, 0.8,'Batch_size = ' + str(batch), transform = ax.transAxes) #horizontalalignment='center', verticalalignment='center', transform = ax.transAxes
+    #plot Hit @1 value
+    plt.text(0.5, 0.75, 'Hit@1 = ' + str(hit1), transform = ax.transAxes)
+    #plot best validation epoch
+    plt.axvline(x = best_epoch, color = 'red')
     plt.xlabel('epochs')
     plt.ylabel('loss')
     plt.title(file)
@@ -63,7 +123,9 @@ def main():
 
     logfile = args.file
     if not os.path.isfile(logfile + '.png'):
-        figure(logfile)
+        data = data_extractor(logfile)
+        figure(logfile, data['hit@1'], data['batch_size'], data['train_loss'], data['valid_loss'], data['best_epoch'])
+        print(data['best_epoch'])
 
 if __name__ == "__main__":
     main()
